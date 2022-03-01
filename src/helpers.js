918b468ba1f8a0b69cfbb72def33ee433359f625
kaydie <info@kaydies.nl>
2022-03-01 01:06:54 +0100

    started queue visibility

--- src/helpers.js
+++ src/helpers.js
@@ -1,119 +1,125 @@
-import { game } from "./gameVariables.js"
-import * as members from "./members.js"
+import { game } from './gameVariables.js';
+import * as members from './members.js';
 
-function createLayer(id){
-    let layer
-    if(document.getElementById(id) == null) {
-        layer = document.createElement("div")
-        layer.classList.add("layer")
-        layer.setAttribute("id", id)
-    } else {
-        layer = document.getElementById(id)
-    }
-    return layer
+function createLayer(id) {
+  let layer;
+  if (document.getElementById(id) == null) {
+    layer = document.createElement('div');
+    layer.classList.add('layer');
+    layer.setAttribute('id', id);
+  } else {
+    layer = document.getElementById(id);
+  }
+  return layer;
 }
 
 function createGrid(element, rows, cols, min, max) {
-    const nav = element.id == "nav" ? true : false
+  const nav = element.id == 'nav' ? true : false;
 
-    if(nav){
-    element.style.gridTemplateColumns = `repeat(${cols}, auto)`
-    const nav = element.id == "nav" ? true : false
+  if (nav) {
+    element.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+    const nav = element.id == 'nav' ? true : false;
     for (let row = 1; row <= rows; row++) {
-        for (let col = 1; col <= cols; col++) {
-            const scale = max - ( (max - min) * ((rows - row + col) / 2) ) / cols
-            const zIndex = (Math.pow(row,2) * rows) + (Math.pow(cols- col,2) * cols)
-            const tile = createTile(row, col, zIndex, scale, nav)
-            element.appendChild(tile)
-       }
+      for (let col = 1; col <= cols; col++) {
+        const scale = max - ((max - min) * ((rows - row + col) / 2)) / cols;
+        const zIndex = Math.pow(row, 2) * rows + Math.pow(cols - col, 2) * cols;
+        const tile = createTile(row, col, zIndex, scale, nav);
+        element.appendChild(tile);
+      }
     }
+  }
+  return element;
 }
-    return element
-}
 
-function createTile(rowIndex, colIndex, zIndex, scale, addEvents = false){
+function createTile(rowIndex, colIndex, zIndex, scale, addEvents = false) {
+  const tile = document.createElement('div');
 
-    const tile = document.createElement("div")
+  tile.classList.add('tile');
 
-    tile.classList.add("tile")
+  tile.dataset.col = colIndex;
+  tile.dataset.row = rowIndex;
 
-    tile.dataset.col = colIndex
-    tile.dataset.row = rowIndex
+  tile.style.transform = `scale(${scale})`;
+  tile.style.zIndex = zIndex;
 
-    tile.style.transform = `scale(${scale})`
-    tile.style.zIndex = zIndex
+  if (addEvents)
+    tile.addEventListener(
+      'click',
+      (e) => {
+        let currentMember = moveToOnStageQueue();
 
-    if(addEvents)
-    tile.addEventListener("click", (e) => {
+        if (currentMember === false) return false;
 
-        let currentMember = moveToOnStageQueue()
+        currentMember.location.currentTile = e.target;
 
-        if (currentMember === false)
-            return false
-
-        currentMember.location.currentTile = e.target
-
-        let queueId = currentMember.anchor.dataset.queueId
-        let scaleValue = e.target.style.transform.match(/scale\(([\d.]*).*\)/)
-        currentMember.location.layer = getRandomLayer() ? getRandomLayer() : null
+        let queueId = currentMember.anchor.dataset.queueId;
+        let scaleValue = e.target.style.transform.match(/scale\(([\d.]*).*\)/);
+        currentMember.location.layer = getRandomLayer()
+          ? getRandomLayer()
+          : null;
 
         currentMember.location.tile = {
-            row: e.target.dataset.row,
-            col: e.target.dataset.col,
-        }
+          row: e.target.dataset.row,
+          col: e.target.dataset.col,
+        };
 
-        members.placeMember(
-            currentMember,
-            scaleValue[1],
-            "click"
-        )
-        members.placeMemberOnBoard(game.onStageQueue[queueId])
+        members.placeMember(currentMember, scaleValue[1], 'click');
+        members.placeMemberOnBoard(game.onStageQueue[queueId]);
+      },
+      false
+    );
 
-    }, false)
-
-    tile.addEventListener("animationend", (e) =>
-        e.target.classList.remove("footprint")
-    )
-    return tile
+  tile.addEventListener('animationend', (e) =>
+    e.target.classList.remove('footprint')
+  );
+  return tile;
 }
 
 function getRandomLayer() {
-const layers = Object.entries(game.board.layers)
-const randomLayerId = layers[Math.ceil(Math.random(layers.length)*layers.length)-1]
-const randomLayer = document.querySelector(`#${randomLayerId[0]}`)
+  const layers = Object.entries(game.board.layers);
+  const randomLayerId =
+    layers[Math.ceil(Math.random(layers.length) * layers.length) - 1];
+  const randomLayer = document.querySelector(`#${randomLayerId[0]}`);
 
-if (randomLayer == null)
-    return false
+  if (randomLayer == null) return false;
 
-return randomLayer
+  return randomLayer;
 }
 
 function fillWaitingQueue(arr, num) {
-    if(num == null)
-        return false;
+  if (num == null) return false;
 
-    for (let i = 1; i <= num; i++) {
+  const memberContainer = document.createElement('div');
+  memberContainer.id = 'member-containter';
 
-        const newMember = members.fetchMemberData()
-        if (newMember === false)
-            return false
+  memberContainer.style.gridTemplateColumns = `repeat(${game.board.members}, auto)`;
 
-            arr.push(newMember)
-    }
+  document.body.appendChild(memberContainer);
+
+  for (let i = 1; i <= num; i++) {
+    const newMemberCell = document.createElement('div');
+    newMemberCell.id = `w-cell-${i}`;
+    newMemberCell.classList.add('cell');
+    memberContainer.appendChild(newMemberCell);
+
+    const newMember = members.fetchMemberData();
+    if (newMember === false) return false;
+
+    arr.push(newMember);
+  }
 }
 
 function moveToOnStageQueue(e) {
+  // if(game.waitingQueue.length === 0)
+  //     return false
 
-    // if(game.waitingQueue.length === 0)
-    //     return false
+  if (game.onStageQueue.length === game.board.members) return false;
 
-    if (game.onStageQueue.length === game.board.members)
-        return false
+  let memberToMove = game.waitingQueue.pop();
 
-    let memberToMove = game.waitingQueue.pop()
+  memberToMove.anchor.dataset.queueId =
+    game.onStageQueue.push(memberToMove) - 1;
 
-    memberToMove.anchor.dataset.queueId = game.onStageQueue.push(memberToMove) - 1
-
-    return memberToMove
+  return memberToMove;
 }
-export { createLayer, createGrid, createTile, fillWaitingQueue }
+export { createLayer, createGrid, createTile, fillWaitingQueue };
